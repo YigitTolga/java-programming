84b2b5193e57e7a0f5101aa0ce0e8008cf10d3c5
tolga <t.yigir35@gmail.com>
2021-05-29 10:22:48 -0400

    Added Classes

--- src/day45_oop/Coffee.java
+++ src/day45_oop/Coffee.java
@@ -1,38 +1,22 @@
 package day45_oop;
 
 public class Coffee {
-    /**
-     * can be 0 - 100 as a percentage
-     */
-    int amount;
     String type;
-
-    @Override
-    public String toString() {
-        return "Coffee{" +
-                "amount=" + amount +
-                ", type='" + type + '\'' +
-                '}';
+    int amount;
+    public void refill(){
+        amount =100;
     }
-
-    public void refill() {
-        amount = 100;
+    public void drink(int someAmount){
+        amount-=someAmount;
     }
-
-    public void drink(int someAmount) {
-        amount -= someAmount;
-    }
-
-    public int getAmount() {
+    public int getAmount(){
         return amount;
     }
-
-    public void setType(String newType) {
+    public void setType(String newType){
         type = newType;
     }
 
     public String getType() {
         return type;
     }
-
 }
--- src/day46_encapsulation/Car.java
+++ src/day46_encapsulation/Car.java
@@ -1,41 +1,37 @@
 package day46_encapsulation;
 
 public class Car {
-    //encapsulated/hidden instance variables
+    // encapsulated/hidden  instance variables
     private String model;
     private int year;
-    private int mileage; //this.mileage = ..
+    private int mileage;
 
-    //setter method for model
+    // setter method
     public void setModel(String carModel) {
         model = carModel;
     }
-    //getter method for model
+
+    // getter method
     public String getModel() {
         return model;
     }
 
-    //setter for year
     public void setYear(int year) {
         this.year = year;
     }
 
-    //getter for year
-    public int getYear() {
+    public Integer getYear() {
         return year;
     }
 
-    //getter setter for mileage
-    //generate toString
-    public int getMileage() {
-        return mileage; //this.mileage; also works fine
-    }
-    //myCar.setMileage(45230);
     public void setMileage(int mileage) {
         this.mileage = mileage;
     }
 
-    @Override
+    public int getMileage() {
+        return mileage;
+    }
+
     public String toString() {
         return "Car{" +
                 "model='" + model + '\'' +
@@ -43,14 +39,6 @@ public class Car {
                 ", mileage=" + mileage +
                 '}';
     }
+
+
 }
-
-
-
-
-
-
-
-
-
-
